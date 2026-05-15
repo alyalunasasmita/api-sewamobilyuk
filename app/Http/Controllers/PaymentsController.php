bec6aa1065615398ca745ff4c99be925ea8e066d
alyaluna sasmita <alyaluna sasmita>
2026-05-15 16:58:46 +0700

    refactor: paymentController

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -55,7 +55,7 @@ class PaymentsController extends Controller
 
         $invoice = $apiInstance->createInvoice($params);
 
-        Payments::create([
+        Payment::create([
             'external_id' => $externalId,
             'amount' => $amount,
             'user_id' => $user->id,
