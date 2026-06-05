7cb5bca827bb4f3f6061b7264a315d1b94763997
alyaluna sasmita <alyaluna sasmita>
2026-06-06 01:18:25 +0700

    callback

--- app/Http/Controllers/ReservationsController.php
+++ app/Http/Controllers/ReservationsController.php
@@ -150,12 +150,6 @@ try {
         'response' => json_encode($snapResponse)
     ]);
 
-    $status = \Midtrans\Transaction::status($orderId);
-    \Log::info('STATUS CEK', [
-        'order_id' => $orderId,
-        'status' => json_encode($status)
-    ]);
-
     $snapToken = $snapResponse->token;
 
     $payment = Payment::create([
